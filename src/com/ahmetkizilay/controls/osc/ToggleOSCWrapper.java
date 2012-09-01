98ac27c2de86d1c741a727383d276bdb3acfade1
ahmet kizilay <ahmet.kizilay@gmail.com>
2012-09-02 02:09:30 +0300

    added new constructor for custom OSC messages

--- src/com/ahmetkizilay/controls/osc/ToggleOSCWrapper.java
+++ src/com/ahmetkizilay/controls/osc/ToggleOSCWrapper.java
@@ -16,6 +16,7 @@ public class ToggleOSCWrapper implements OnCheckedChangeListener{
 	private QuickOSCActivity parentActivity;
 	private String messageToggleOn;
 	private String messageToggleOff;
+	private String name;
 	
 	/**
 	 * Default constructor publicly available for other classes.
@@ -28,11 +29,29 @@ public class ToggleOSCWrapper implements OnCheckedChangeListener{
 		return new ToggleOSCWrapper(name, button, parentActivity);
 	}
 	
+	public static ToggleOSCWrapper createInstance(String name, String msgToggleOn, String msgToggleOff, ToggleButton button, QuickOSCActivity parentActivity) {
+		return new ToggleOSCWrapper(name, msgToggleOn, msgToggleOff, button, parentActivity);
+	}
+	
 	private ToggleOSCWrapper(String name, ToggleButton button, QuickOSCActivity parentActivity) {
 		this.parentActivity = parentActivity;
-		
+		this.name = name;
 		this.messageToggleOn = "/" + name + "/1";
 		this.messageToggleOff = "/" + name + "/0";
+		
+		
+		button.setOnCheckedChangeListener(this);
+		button.setText(name);
+		button.setTextOn(name);
+		button.setTextOff(name);
+	}
+	
+	private ToggleOSCWrapper(String name, String msgToggleOn, String msgToggleOff, ToggleButton button, QuickOSCActivity parentActivity) {
+		this.parentActivity = parentActivity;
+		this.name = name;
+		this.messageToggleOn = (msgToggleOn == null || msgToggleOn.equals("")) ? "/" + name + "/1" : msgToggleOn;
+		this.messageToggleOff = (msgToggleOff == null || msgToggleOff.equals("")) ? "/" + name + "/0" : msgToggleOff;
+		
 		
 		button.setOnCheckedChangeListener(this);
 		button.setText(name);
@@ -73,5 +92,12 @@ public class ToggleOSCWrapper implements OnCheckedChangeListener{
 		this.messageToggleOff = messageToggleOff;
 	}
 	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
