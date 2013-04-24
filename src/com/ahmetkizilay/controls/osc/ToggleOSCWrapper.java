74b46df51398b03cbc88a6b50e7b1642a0cf7784
ahmet kizilay <ahmet.kizilay@gmail.com>
2013-04-24 23:45:39 +0300

    messages are broken down as Address and Arguments

--- src/com/ahmetkizilay/controls/osc/ToggleOSCWrapper.java
+++ src/com/ahmetkizilay/controls/osc/ToggleOSCWrapper.java
@@ -14,9 +14,17 @@ import android.widget.ToggleButton;
 public class ToggleOSCWrapper implements OnCheckedChangeListener{
 	
 	private QuickOSCActivity parentActivity;
-	private String messageToggleOn;
-	private String messageToggleOff;
+	
+	private String messageToggleOnAddr;
+	private Object[] messageToggleOnArgs;
+	private String messageToggleOnRaw;
+	
+	private String messageToggleOffAddr;
+	private Object[] messageToggleOffArgs;
+	private String messageToggleOffRaw;
 	private String name;
+	
+	private int index;
 	
 	/**
 	 * Default constructor publicly available for other classes.
@@ -25,19 +33,26 @@ public class ToggleOSCWrapper implements OnCheckedChangeListener{
 	 * @param parentActivity
 	 * @return
 	 */
-	public static ToggleOSCWrapper createInstance(String name, ToggleButton button, QuickOSCActivity parentActivity) {
-		return new ToggleOSCWrapper(name, button, parentActivity);
+	public static ToggleOSCWrapper createInstance(int index, String name, ToggleButton button, QuickOSCActivity parentActivity) {
+		return new ToggleOSCWrapper(index, name, button, parentActivity);
 	}
 	
-	public static ToggleOSCWrapper createInstance(String name, String msgToggleOn, String msgToggleOff, ToggleButton button, QuickOSCActivity parentActivity) {
-		return new ToggleOSCWrapper(name, msgToggleOn, msgToggleOff, button, parentActivity);
+	public static ToggleOSCWrapper createInstance(int index, String name, String msgToggleOn, String msgToggleOff, ToggleButton button, QuickOSCActivity parentActivity) {
+		return new ToggleOSCWrapper(index, name, msgToggleOn, msgToggleOff, button, parentActivity);
 	}
 	
-	private ToggleOSCWrapper(String name, ToggleButton button, QuickOSCActivity parentActivity) {
+	private ToggleOSCWrapper(int index, String name, ToggleButton button, QuickOSCActivity parentActivity) {
+		this.index = index;
 		this.parentActivity = parentActivity;
 		this.name = name;
-		this.messageToggleOn = "/" + name + "/1";
-		this.messageToggleOff = "/" + name + "/0";
+		
+		this.messageToggleOnAddr = "/" + name + "/1";
+		this.messageToggleOnArgs = null;
+		this.messageToggleOnRaw = this.messageToggleOnAddr;
+		
+		this.messageToggleOffAddr = "/" + name + "/0";
+		this.messageToggleOffArgs = null;
+		this.messageToggleOffRaw = this.messageToggleOffAddr;
 		
 		
 		button.setOnCheckedChangeListener(this);
@@ -46,11 +61,13 @@ public class ToggleOSCWrapper implements OnCheckedChangeListener{
 		button.setTextOff(name);
 	}
 	
-	private ToggleOSCWrapper(String name, String msgToggleOn, String msgToggleOff, ToggleButton button, QuickOSCActivity parentActivity) {
+	private ToggleOSCWrapper(int index, String name, String msgToggleOn, String msgToggleOff, ToggleButton button, QuickOSCActivity parentActivity) {
+		this.index = index;
 		this.parentActivity = parentActivity;
 		this.name = name;
-		this.messageToggleOn = (msgToggleOn == null || msgToggleOn.equals("")) ? "/" + name + "/1" : msgToggleOn;
-		this.messageToggleOff = (msgToggleOff == null || msgToggleOff.equals("")) ? "/" + name + "/0" : msgToggleOff;
+
+		this.setMessageToggleOn(msgToggleOn);
+		this.setMessageToggleOff(msgToggleOff);
 		
 		
 		button.setOnCheckedChangeListener(this);
@@ -66,30 +83,62 @@ public class ToggleOSCWrapper implements OnCheckedChangeListener{
 		}	
 		
 		if(isChecked) {
-			parentActivity.sendOSC(messageToggleOn);
-			parentActivity.setDebugMessage(messageToggleOn);
+			parentActivity.sendOSC(this.messageToggleOnAddr, this.messageToggleOnArgs);
+			parentActivity.setDebugMessage(this.messageToggleOnRaw);
 		}
 		else {
-			parentActivity.sendOSC(messageToggleOff);
-			parentActivity.setDebugMessage(messageToggleOff);
+			parentActivity.sendOSC(this.messageToggleOffAddr, this.messageToggleOffArgs);
+			parentActivity.setDebugMessage(this.messageToggleOffRaw);
 		}
 
 	}
 
-	public String getMessageToggleOn() {
-		return messageToggleOn;
+	public String getMessageToggleOnRaw() {
+		return this.messageToggleOnRaw;
 	}
 
 	public void setMessageToggleOn(String messageToggleOn) {
-		this.messageToggleOn = messageToggleOn;
+		if(messageToggleOn == null || messageToggleOn.equals("")) {
+			this.messageToggleOnAddr = "/" + name + "/1";
+			this.messageToggleOnArgs = null;
+			this.messageToggleOnRaw = this.messageToggleOnAddr;
+		}
+		else {
+			this.messageToggleOnRaw = messageToggleOn;
+			
+			String[] messageToggleOnParts = messageToggleOn.split(" ");
+			this.messageToggleOnAddr = messageToggleOnParts[0];
+			if(messageToggleOnParts.length > 0) {
+				this.messageToggleOnArgs = new Object[messageToggleOnParts.length - 1];
+				for(int i = 1; i < messageToggleOnParts.length; i++) {
+					this.messageToggleOnArgs[i - 1] = Utils.simpleParse(messageToggleOnParts[i]);
+				}
+			}
+		}
 	}
 
-	public String getMessageToggleOff() {
-		return messageToggleOff;
+	public String getMessageToggleOffRaw() {
+		return messageToggleOffRaw;
 	}
 
 	public void setMessageToggleOff(String messageToggleOff) {
-		this.messageToggleOff = messageToggleOff;
+		if(messageToggleOff == null || messageToggleOff.equals("")) {
+			this.messageToggleOffAddr = "/" + name + "/0";
+			this.messageToggleOffArgs = null;
+			this.messageToggleOffRaw = this.messageToggleOffAddr;
+		}
+		else {
+			this.messageToggleOffRaw = messageToggleOff;
+			
+			String[] messageToggleOffParts = messageToggleOff.split(" ");
+			this.messageToggleOffAddr = messageToggleOffParts[0];
+			if(messageToggleOffParts.length > 0) {
+				this.messageToggleOffArgs = new Object[messageToggleOffParts.length - 1];
+				for(int i = 1; i < messageToggleOffParts.length; i++) {
+					this.messageToggleOffArgs[i - 1] = Utils.simpleParse(messageToggleOffParts[i]);
+				}
+			}
+		}
 	}
 	
 	public String getName() {
@@ -98,6 +147,10 @@ public class ToggleOSCWrapper implements OnCheckedChangeListener{
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public int getIndex() {
+		return this.index;
 	}
 	
 }
